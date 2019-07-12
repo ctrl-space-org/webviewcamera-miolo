8a4afc141540ecc171639c992e58989e419f8cdd
Billzee <guilhermezordan@outlook.com>
2019-07-11 23:55:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const openCamera = () => {
+    window.ReactNativeWebView.postMessage("camera!");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button style={{ padding: 20 }} type="button" onClick={openCamera}>
+          Open Camera
+        </button>
       </header>
     </div>
   );
